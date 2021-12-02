a1553ac7ea5bd328191cf6fa2ed18561d8441a7d
Martu <martiague97@hotmail.com>
2021-12-01 23:57:59 -0300

    adaptaciones

--- app.js
+++ app.js
@@ -7,30 +7,29 @@ const publicPath = path.join(__dirname, "/public")
 app.use(express.static(publicPath))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'))
-  })
+})
 
 app.get('/productCart', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productCart.html'))
-  })
+})
 
 app.get('/productDetail', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/productDetail.html'))
-  })
+})
 
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'))
-  })
-
-app.post('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'))
 })
 
-app.listen(process.env.PORT || 3000
-  , () => {
-  console.log(`listening at http://localhost:${port}`)
+app.post('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`listening at http://localhost:${port}`)
 })
