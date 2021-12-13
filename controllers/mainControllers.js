919308bc490e2306924069bd712a5b2d5af6aebb
Martu <martiague97@hotmail.com>
2021-12-13 19:57:15 -0300

    checkpoint

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const controllers = {
-    index:(req, res) => {res.render(path.resolve(__dirname + '/../views/users/index'))},
-    login:(req, res) => {res.render(path.resolve(__dirname + '/../views/users/login'))},
-    register:(req, res) => {res.render(path.resolve(__dirname + '/../views/users/register'))},
-    productDetail:(req, res) => {res.render(path.resolve(__dirname + '/../views/users/productDetail'))},
-    productCart:(req, res) => {res.render(path.resolve(__dirname + '/../views/users/productcart'))},
+    index:(req, res) => {res.render(path.resolve('index'))},
+    login:(req, res) => {res.render(path.resolve('login'))},
+    register:(req, res) => {res.render(path.resolve('register'))},
+    productDetail:(req, res) => {res.render(path.resolve('productDetail'))},
+    productCart:(req, res) => {res.render(path.resolve('productCart'))},
 }
 
 module.exports= controllers
