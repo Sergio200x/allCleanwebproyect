3049eed1d8a111ae2dc08b409da00e84030b4da0
diego856 <diegomarco@hotmail.com.ar>
2021-12-18 19:03:33 -0300

    agregando baes de datos Json + View products.ejs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const loginRoutes = require('./routes/loginRoutes')
 const productDetailRoutes = require('./routes/productDetailRoutes')
 const productCartRoutes = require('./routes/productCartRoutes')
 const registerRoutes = require('./routes/registerRoutes')
+const productsRoutes = require('./routes/productsRoutes')
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
@@ -18,6 +19,7 @@ app.use('/login', loginRoutes)
 app.use('/productDetail', productDetailRoutes)
 app.use('/productCart', productCartRoutes)
 app.use('/register', registerRoutes)
+app.use('/products', productsRoutes)
 
 app.listen(process.env.PORT || port, () => {
     console.log(`listening at http://localhost:${port}`)
--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,10 +1,15 @@
 const { validationResult } = require("express-validator")
+const jsonTable = require('../database/jsonTable');
 
+const products = jsonTable('products_json');
 
 const controllers = {
     index:(req, res) => {res.render('index')},
+
     login:(req, res) => {res.render('login')},
+
     register:(req, res) => {res.render('users/register')},
+
     processRegister: (req, res) => {
         const resultvalidations = validationResult(req);
         if(!resultvalidations.isEmpty()){
@@ -16,8 +21,17 @@ const controllers = {
 
         return res.send('Pasaste las Validaciones, Falta que Sergio arme la Vista!!!')
     },
+
     productDetail:(req, res) => {res.render('products/productDetail')},
+
     productCart:(req, res) => {res.render('products/productCart')},
+
+    products: (req, res) => {
+
+        let productList = products.all()
+
+        res.render('products/products',  { productList });
+    },
 }
 
 module.exports= controllers
