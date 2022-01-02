bee01cb73c7ed1be77fcc559f28c42cd952cdaa4
diego856 <diegomarco@hotmail.com.ar>
2022-01-02 17:27:01 -0300

    estilo mejorado al btn slider + transision 5s banner

--- public/js/index-slider.js
+++ public/js/index-slider.js
@@ -13,12 +13,12 @@ function next()
     const first = 0;
     let sliderSectionFirst = document.querySelectorAll(".index-slider-section")[first];
     slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s";
+    slider.style.transition = "all 1s";
     setTimeout(function(){
         slider.style.transition ="none";
         slider.insertAdjacentElement("beforeend",sliderSectionFirst);
         slider.style.marginLeft = "-100%";
-    }, 500);
+    }, 1000);
 }
 
 function prev()
@@ -26,12 +26,12 @@ function prev()
     let sliderSection = document.querySelectorAll(".index-slider-section");
     let sliderSectionLast = sliderSection[sliderSection.length -1];
     slider.style.marginLeft = "0";
-    slider.style.transition = "all 0.5s";
+    slider.style.transition = "all 1s";
     setTimeout(function(){
         slider.style.transition ="none";
         slider.insertAdjacentElement("afterbegin", sliderSectionLast);
         slider.style.marginLeft = "-100%";
-    }, 500);
+    }, 1000);
 }
 
 btnRight.addEventListener('click', function(){
