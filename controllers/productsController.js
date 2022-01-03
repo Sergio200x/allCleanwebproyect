13cac9b7e3a48202be1b861d64a7b01640e84b7e
diego856 <diegomarco@hotmail.com.ar>
2022-01-03 20:28:59 -0300

    configurando btn eliminar y editar

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,7 +2,6 @@ const { validationResult } = require("express-validator")
 const jsonTable = require('../database/jsonTable');
 const products = jsonTable('products');
 
-const productList = products.all()
 
 const productsControllers = {
     products: (req, res) => {
@@ -12,9 +11,8 @@ const productsControllers = {
     },
 
     productDetail:(req, res) => {
-        
         const idproduct = req.params.id
-
+        const productList = products.all()
         const productfound = productList.find(product => product.id == idproduct)
 
         res.render('products/productDetail',{productfound: productfound});
