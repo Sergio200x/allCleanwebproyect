3a984afed763c71fefcf43bfc1d4afe689bad219
diego856 <diegomarco@hotmail.com.ar>
2022-02-02 22:01:16 -0300

    revisando gitingnore

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     "password": "secret",
     "database": "allclean",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "mariadb"
   },
   "test": {
     "username": "root",
