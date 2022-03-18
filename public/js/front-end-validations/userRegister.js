5dda5b38d64ade5a848bc66d5f05190edec397c6
Sergio200x <sergio_200x@hotmail.com>
2022-03-17 23:36:21 -0300

    casi temrinando validacion por front register

--- public/js/front-end-validations/userRegister.js
+++ public/js/front-end-validations/userRegister.js
@@ -1,43 +1,66 @@
 window.addEventListener('load',function(){
 
-    let form = document.querySelector('form')
+let form = document.querySelector('#register-create-form')
+let submit = document.querySelector('.register-submit')
+
+console.log(submit.value)
+
+form.addEventListener('submit',function(e){
+    let inputName = document.querySelector('#Name')
+    let inputLastName = document.querySelector('#LastName')
+    let inputUserName = document.querySelector('#userName')
+    let inputMail = document.querySelector('#Mail')
+    let inputDate = document.querySelector('#Date')
+    let inputPass= document.querySelector('#password')
+    let inputPassConfirm = document.querySelector('#confirm_password')
+
+     let error=[]
     
+    if(inputName.value=="")
+    {        
+        error.push('El Nombre no puede estar Vacio') 
+    }
+    else if (inputLastName.value=="")
+    {
+        error.push('El Apellido no puede estar Vacio')  
+    }    
+    else if (inputUserName.value=="")
+    {
+        error.push('El Nombre de usuario no puede estar Vacio') 
+    }
+    else if (inputMail.value=="")
+    {
+        error.push('El Mail no puede estar Vacio') 
+    }
+    else if (inputDate.value=="")
+    {
+        error.push('La fecha no puede estar Vacia')
+    }
+    else if (inputPass.value=="")
+    {
+        error.push('La contraseña no puede estar Vacia ni tener menos de 8 digitos') 
+    }
+    else if (inputPassConfirm.value=="")
+    {
+        error.push('La a anterior')
+    } 
+   
     
-    form.addEventListener('submit',function(e){
-        let inputName1 = document.querySelector('#Name')
-        let inputLastName1 = document.querySelector('#LastNamee')
-        let inputUserName1 = document.querySelector('#userName')
-        let inputDate1 = document.querySelector('#Date')
-        let inputPass1 = document.querySelector('#password')
-        let inputPassConfirm1 = document.querySelector('#confirm_password')
-        let img1 = document.querySelector('#file')
+    if(error.length>0)
+    {
+        for(errors of error)
+        {
+            swal("Aviso!!!", errors, "error");
+        }
+        e.preventDefault()
+    }
+    else
+    {
+        swal("Excelente!!!", "Te registraste exitosamente", "success");
+    }
+
     
-        error=[]
-        console.log(inputName1.value)
-    
-        if(inputName1.value=="")
-        {
-            error.push('El Nombre no puede estar Vacio')      
-        }
-        else if (inputLastName1.value=="")
-        {
-            error.push('El Apellido no puede estar Vacio')            
-        }
-        else if (inputUserName1.value=="")
-        {
-            error.push('El Nombre de usuario no puede estar Vacio')             
-        }
-        else if (inputDate1.value=="")
-        {
-            error.push('La fecha no puede estar Vacia')
-        }
-        else if (inputPass1.value=="" & inputPass1.value.lenght>8)
-        {
-            error.push('La contraseña no puede estar Vacia ni tener menos de 8 digitos')     
-        }
-        else if (inputPassConfirm1.value=="")
-        {
-            error.push('La a anterior')    
-        }
-      })
-     })
+
+
+  })
+ })
